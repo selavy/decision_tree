ec7015dbb6e03613247bed637f66357e0058aa49
Peter Lesslie <pclesslie@gmail.com>
2018-02-11 21:01:37 -0600

    Finish cython v0.1

--- fast_decision_tree.pyx
+++ fast_decision_tree.pyx
@@ -2,15 +2,19 @@ cimport cython
 cimport numpy as np
 import numpy as np
 
-def eval_tree(object tree, np.ndarray[double] xs):
-#    cdef np.ndarray[np.int64] children_left = tree.children_left
-#    cdef np.ndarray[np.int64] children_right = tree.children_right
-    
-    node_id = 0
-    while tree.children_left[node_id] >= 0:
-        if xs[tree.feature[node_id]] <= tree.threshold[node_id]:
-            node_id = tree.children_left[node_id]
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.nonecheck(False)
+def eval_tree(object tree, np.ndarray[double, ndim=1, mode="c"] xs):
+    cdef np.ndarray[np.int64_t] children_left = tree.children_left
+    cdef np.ndarray[np.int64_t] children_right = tree.children_right
+    cdef np.ndarray[np.int64_t] feature = tree.feature
+    cdef np.ndarray[np.float64_t] threshold = tree.threshold
+    cdef int node_id = 0
+    while children_left[node_id] >= 0:
+        if xs[feature[node_id]] <= threshold[node_id]:
+            node_id = children_left[node_id]
         else:
-            node_id = tree.children_right[node_id]
+            node_id = children_right[node_id]
     return node_id
     
