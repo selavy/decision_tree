5ae72fac0b1de4c09d79a659a15eaf0728fb0117
Peter Lesslie <pclesslie@gmail.com>
2018-02-11 21:53:34 -0600

    Try race with 500 estimators...

--- race.py
+++ race.py
@@ -40,7 +40,7 @@ y = y.flatten()
 
 from sklearn.ensemble import RandomForestClassifier
 
-clf = RandomForestClassifier(n_estimators=2,
+clf = RandomForestClassifier(n_estimators=500,
                              criterion="gini",
                              max_depth=5,
                              min_samples_leaf=2,
@@ -55,27 +55,27 @@ y_proba = clf.predict_proba(X)
 
 # %%
 
-# random forest is mean of probas
-proba0 = clf.estimators_[0].predict_proba(X)
-proba1 = clf.estimators_[1].predict_proba(X)
-proba = proba0 + proba1
-result = proba / 2.
-print(np.allclose(result, y_proba))
-
-# %%
-
-X32 = X.astype(np.float32)
-p0 = clf.estimators_[0].tree_.predict(X32)
-normalizer = np.sum(p0, axis=1)[:, np.newaxis]
-normalizer[normalizer == 0.0] = 1.0
-proba0 = p0 / normalizer
-p1 = clf.estimators_[1].tree_.predict(X32)
-normalizer = np.sum(p1, axis=1)[:, np.newaxis]
-normalizer[normalizer == 0.0] = 1.0
-proba1 = p1 / normalizer
-proba = proba0 + proba1
-result = proba / 2.
-print(np.allclose(result, y_proba))
+## random forest is mean of probas
+#proba0 = clf.estimators_[0].predict_proba(X)
+#proba1 = clf.estimators_[1].predict_proba(X)
+#proba = proba0 + proba1
+#result = proba / 2.
+#print(np.allclose(result, y_proba))
+#
+## %%
+#
+#X32 = X.astype(np.float32)
+#p0 = clf.estimators_[0].tree_.predict(X32)
+#normalizer = np.sum(p0, axis=1)[:, np.newaxis]
+#normalizer[normalizer == 0.0] = 1.0
+#proba0 = p0 / normalizer
+#p1 = clf.estimators_[1].tree_.predict(X32)
+#normalizer = np.sum(p1, axis=1)[:, np.newaxis]
+#normalizer[normalizer == 0.0] = 1.0
+#proba1 = p1 / normalizer
+#proba = proba0 + proba1
+#result = proba / 2.
+#print(np.allclose(result, y_proba))
 
 # %% ---- vectorized version
 
@@ -88,40 +88,43 @@ print("Result #1: {}".format(result))
       
 # %% ---- non-vectorized version
 
-start = time.time()
-y_pred2 = np.zeros(len(X), dtype=np.double)
-for i in range(len(X)):
-    y_pred2[i] = clf.predict(X[i, :].reshape(1, -1))
-end = time.time()
-print("Took {:0.2f} seconds.".format(end - start))
-result = np.allclose(y_pred, y_pred2)
-print("Result #2: {}".format(result))
+if False:
+    start = time.time()
+    y_pred2 = np.zeros(len(X), dtype=np.double)
+    for i in range(len(X)):
+        y_pred2[i] = clf.predict(X[i, :].reshape(1, -1))
+    end = time.time()
+    print("Took {:0.2f} seconds.".format(end - start))
+    result = np.allclose(y_pred, y_pred2)
+    print("Result #2: {}".format(result))
 
 # %% ---- non-vectorized version #2
 
-start = time.time()
-y_pred2 = np.zeros(len(X), dtype=np.double)
-vals = np.zeros((1, X.shape[1]), dtype=np.double)
-for i in range(len(X)):
-    for j in range(len(vals)):
-        vals[0][j] = X[i, j]
-    y_pred2[i] = clf.predict(vals)
-end = time.time()
-print("Took {:0.2f} seconds.".format(end - start))
-result = np.allclose(y_pred, y_pred2)
-print("Result #2: {}".format(result))
+if False:
+    start = time.time()
+    y_pred2 = np.zeros(len(X), dtype=np.double)
+    vals = np.zeros((1, X.shape[1]), dtype=np.double)
+    for i in range(len(X)):
+        for j in range(len(vals)):
+            vals[0][j] = X[i, j]
+        y_pred2[i] = clf.predict(vals)
+    end = time.time()
+    print("Took {:0.2f} seconds.".format(end - start))
+    result = np.allclose(y_pred, y_pred2)
+    print("Result #2: {}".format(result))
       
 # %% ---- non-vectorized version #3: convert to float32 ahead of time
 
-start = time.time()
-X32 = X.astype(np.float32)
-y_pred2 = np.zeros(len(X32), dtype=np.double)
-for i in range(len(X32)):
-    y_pred2[i] = clf.predict(X32[i, :].reshape(1, -1))
-end = time.time()
-print("Took {:0.2f} seconds.".format(end - start))
-result = np.allclose(y_pred, y_pred2)
-print("Result #2: {}".format(result))
+if False:
+    start = time.time()
+    X32 = X.astype(np.float32)
+    y_pred2 = np.zeros(len(X32), dtype=np.double)
+    for i in range(len(X32)):
+        y_pred2[i] = clf.predict(X32[i, :].reshape(1, -1))
+    end = time.time()
+    print("Took {:0.2f} seconds.".format(end - start))
+    result = np.allclose(y_pred, y_pred2)
+    print("Result #2: {}".format(result))
     
 # %% ---- using cython version
 
@@ -133,15 +136,5 @@ end = time.time()
 print("Took {:0.2f} seconds.".format(end - start))
 result = np.allclose(y_pred, y_pred2)
 print("Result #2: {}".format(result))
-            
-#start = time.time()
-#y_pred2 = np.zeros(len(X), dtype=np.double)
-#tree = clf.estimators_[0].tree_
-#for i in range(len(X)):
-#    y_pred2[i] = eval_tree(tree, X[i, :])
-#end = time.time()
-#print("Took {:0.2f} seconds.".format(end - start))
-#result = np.allclose(y_pred, y_pred2)
-#print("Result #2: {}".format(result))
 
 # %%
